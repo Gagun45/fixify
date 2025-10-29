f8f3b3e4d3e4729efb585aeb0b6aead6966eb2ce
Gagun45 <selyanchyn45@gmail.com>
2025-10-29 13:40:35 +0200

    contact animation set

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,10 +1,20 @@
+import useInView from "../../hooks/useInView";
 import styles from "./Contact.module.css";
 import FormContainer from "./FormContainer/FormContainer";
 import TextContainer from "./TextContainer/TextContainer";
 
 export default function Contact() {
+  const { ref, visible } = useInView();
   return (
-    <section className={styles.contact} id="contact">
+    <section
+      className={styles.contact}
+      id="contact"
+      style={{
+        opacity: visible ? 1 : 0,
+        transition: "opacity 0.8s ease",
+      }}
+      ref={ref}
+    >
       <div className="globalContentWrapper">
         <h2>Get in Touch</h2>
         <div className={styles.grid}>
